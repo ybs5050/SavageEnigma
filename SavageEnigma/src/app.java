90adc48b131a36b895907d86e61a674cabe7b0b1
Youngmin Son <bfid111@gmail.com>
2017-09-28 23:33:35 -0400

    push

---
+++ SavageEnigma/src/app.java
@@ -0,0 +1,33 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import login.LoginController;
+
+/*
+ * IST 440W Team 6
+ * Project Savage Enigma
+ * Application Main Class
+ */
+
+/**
+ *
+ * @author Youngmin
+ */
+public class app extends Application {
+    
+    private static Stage base;
+    private static Scene login;
+    private static LoginController loginController;
+    
+    /**
+     * Stage method for Main Menu 
+     * @param primaryStage Give by default
+     * @throws java.lang.Exception 
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+
+}
---
+++ SavageEnigma/src/login/LoginController.java
@@ -0,0 +1,28 @@
+/*
+ * IST 440W Team 6
+ * Project Savage Enigma
+ */
+
+package login;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller Class
+ * @author Youngmin
+ */
+public class LoginController implements Initializable{
+    
+    /**
+     * Initializes the controller class
+     * @param location given/none by default
+     * @param resources given/none by default
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
